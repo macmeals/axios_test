0616cf02837898126c63c1c1d11c8fe32c71c437
macmeals <loseworld9731@gmail.com>
2022-03-29 00:40:40 +0900

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 
-const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+const url = "https://pokeapi.co/api/v2/pokemon-form/25/"
+const url2 = ""
 const fetchData = async () => {
   const result = await axios.get(url)
-  console.log(result)
+  console.log(result) //ピカチュウの全体？情報
+  console.log(result.data.sprites)　//ピカチュウ全ての画像
+  console.log(result.data.sprites.front_default)　//ピカチュウ正面図
+  console.log(result.data.pokemon.name)　//ピカチュウ名前
+  
 };
 
 fetchData();
